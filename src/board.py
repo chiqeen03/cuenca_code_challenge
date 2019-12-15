350867186cdc1fd273a5b7dac1aa40585f40d592
Manuel Avalos Tovar <manuelavalos97@gmail.com>
2019-12-15 14:42:16 -0600

    Method in board added to check if a position is safe

--- src/board.py
+++ src/board.py
@@ -12,12 +12,12 @@ class Board:
         n = self.n
         array = self.array
         
-        nm = "    "
+        nm = "  y "
         for i in range(n):
             nm += str(i) + " "
         print (nm)
 
-        line = "   -"
+        line = "x  -"
         for i in range(n):
             if(i != n-1):
                 line += "--"
@@ -37,23 +37,83 @@ class Board:
 
     # writes a queen (1) at a certain position (x, y)
     def place_queen_at(self, x, y):
-        if (x >= self.n or y >= self.n or x < 0 or y < 0):
-            return False
-        self.array[x][y] = 1
-        return True
+        if self.in_bounds(x, y):
+            self.array[x][y] = 1
 
     # deletes a queen at a certain position (x, y)
     def delete_queen_at(self, x, y):
-        if (x >= self.n or y >= self.n or x < 0 or y < 0):
-            return False
-        self.array[x][y] = 0
-        return True
+        if self.in_bounds(x, y):
+            self.array[x][y] = 0
 
     # returns what value is at a certain position (x, y)
     def get_value_at(self, x, y):
+        if self.in_bounds(x, y):
+            return self.array[x][y]
+        return -1
+
+    # simple method to check if (x, y) is in bounds
+    def in_bounds(self, x, y):
         if (x >= self.n or y >= self.n or x < 0 or y < 0):
             return False
-        return self.array[x][y]
         return True
+
+    # checks if there are dangerous queens previously placed in the board
+    def is_safe(self, x, y):
+        if (self.in_bounds(x, y)):
+            # checks if there are queens at the row of the position
+            for i in range(self.n):
+                if self.array[x][i]:
+                    print(f"(-) there is a queen at: {x}, {i}")
+                    return False
+
+            # checks if there are queens at the col of the position
+            for i in range(self.n):
+                if self.array[i][y]:
+                    print(f"(|) there is a queen at: {i}, {y}")
+                    return False
+
+            # checks right-up diagonal (/)
+            i = x
+            j = y
+            while i >= 0 and j < self.n:
+                if self.array[i][j]:
+                    print(f"(ru/)there is a queen at: {i}, {j}")
+                    return False
+                i -= 1
+                j += 1
+
+            # checks right-down diagonal (\)
+            i = x
+            j = y
+            while i < self.n and j < self.n:
+                if self.array[i][j]:
+                    print(f"(rd\) there is a queen at: {i}, {j}")
+                    return False
+                i += 1
+                j += 1
+
+            # checks left-up diagonal (\)
+            i = x
+            j = y
+            while i >= 0 and j >= 0:
+                if self.array[i][j]:
+                    print(f"(lu\) there is a queen at: {i}, {j}")
+                    return False
+                i -= 1
+                j -= 1
+
+            # checks left-down diagonal (/)
+            i = x
+            j = y
+            while i < self.n and j >= 0:
+                if self.array[i][j]:
+                    print(f"(ld/) there is a queen at: {i}, {j}")
+                    return False
+                i += 1
+                j -= 1
+
+            return True
+        print("is safe: out of bounds")
+        return False
 
     
--- src/main.py
+++ src/main.py
@@ -4,6 +4,13 @@ n = 4
 board = Board(n)
 board.print_array()
 
-board.place_queen_at(3,7)
+# board.place_queen_at(2,0)
+# board.place_queen_at(0,2)
+board.place_queen_at(0,0)
 
 board.print_array()
+
+# print(board.is_safe(2, 3))
+# print(board.is_safe(3, 2))
+# print(board.is_safe(1, 3))
+print(board.is_safe(3, 3))
